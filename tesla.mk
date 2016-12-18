3037437c9797e418913bc4aa19d3775c262af936
Sandeep Sethi <adeveloper79@gmail.com>
2016-12-18 07:43:46 +0530

    Update and rename cm.mk to tesla.mk

--- tesla.mk
+++ tesla.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/lenovo/aio_row/device_aio_row.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := aio_row
-PRODUCT_NAME := cm_aio_row
+PRODUCT_NAME := tesla_aio_row
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := A7000-a
 PRODUCT_MANUFACTURER := Lenovo
